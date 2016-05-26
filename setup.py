473283006d3bd376b49ed797b93c35c0e787d2c4
Yigit Ozen <canyigitozen@gmail.com>
2016-05-26 13:34:19 +0300

    Updated setup.py description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='yosun',
     version='0.3.0',
-    description='Simple pub/sub client on top of Kombu',
+    description='Simple pub/sub utility for Kombu',
     author='Yigit Ozen',
     author_email='ozen@computer.org',
     url='http://ozen.github.io',
